868df0a22374500ed7937b1175928edd7e099910
arauta12 <andreirauta234@gmail.com>
2025-08-06 15:00:14 -0400

    Change base path

--- frontend/src/components/ImageLink/ImageLink.jsx
+++ frontend/src/components/ImageLink/ImageLink.jsx
@@ -1,19 +1,11 @@
-import './ImageLink.css'
+import "./ImageLink.css";
 
 function ImageLink({ imageName, url, name, customStyles }) {
-    return (
-        <a 
-            className="web-container"
-            href={url}
-            target="_blank"
-        >
-            <img 
-                src={`/images/${imageName}`} 
-                alt={name} 
-                style={customStyles}
-            />
-        </a>
-    )
+	return (
+		<a className="web-container" href={url} target="_blank">
+			<img src={`/images/${imageName}`} alt={name} style={customStyles} />
+		</a>
+	);
 }
 
 export default ImageLink;
